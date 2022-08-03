0306193583fd02690c46b18f6fd75f5430b4c66f
kamal-ganiev <kamal.ganiev999@gmail.com>
2022-08-02 23:53:05 -0400

    Add Esc close and outsid click close features to modal

--- src/script/Modal.js
+++ src/script/Modal.js
@@ -4,29 +4,32 @@ export default class Modal {
     this._modal = document.querySelector(this._modalSelector);
   }
 
-  _handleEscClose(evt) {
+  _handleEscClose = (evt) => {
     if (evt.key === "Escape") {
       this.close();
     }
-  }
+  };
 
-  _handleOutsideClickClose(evt) {
+  _handleOutsideClickClose = (evt) => {
     if (evt.target === evt.currentTarget) {
       this.close();
     }
-  }
+  };
 
   open() {
     this._modal.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
     this._modal.classList.remove("modal_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
   setEventListeners() {
     this._modal
       .querySelector(".modal__close-button")
       .addEventListener("click", () => this.close());
+    this._modal.addEventListener("mousedown", this._handleOutsideClickClose);
   }
 }
